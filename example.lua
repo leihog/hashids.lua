a15cba2a17a99eb32f2b1b112e30946a257b266d
Leif Högberg <leihog@gmail.com>
2014-09-12 09:46:10 +0200

    updated readme and examples to reflect changes in v1.0

--- example.lua
+++ example.lua
@@ -5,12 +5,12 @@ local hashids = require("hashids");
 local h = hashids.new(nil, 8, alphabet);
 
 local ts = os.time();
-print(h:encrypt(34, 2525));
+print(h:encode(34, 2525));
 
 local hash;
 for i = 1, 10000 do
-	h:decrypt(h:encrypt(i, 2525));
+	h:decode(h:encode(i, 2525));
 end
 
-print(h:encrypt(1337, 10, 666));
+print(h:encode(1337, 10, 666));
 print("elapsed time (in seconds):", os.time() - ts);
