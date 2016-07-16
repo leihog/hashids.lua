1999de3215af7e3f273b15d132ea444c6e58ab22
un.def <un.def@ya.ru>
2016-07-16 20:41:28 +0300

    add more tests

--- spec/fixtures.lua
+++ spec/fixtures.lua
@@ -1,11 +1,24 @@
 return {
-  jR = 1,
-  vm = 10,
-  g56 = 100,
-  ZgwE = 33333,
-  gE3Y95MDwl = 33333e10,
-  lYfo = {1, 2},
-  kQcn = {0, 2},
-  kQc6cV = {0, 0, 2},
-  oYxu6xh3ytXMO6 = {123, 456, 789, 1000000},
+
+  [''] = {
+    jR = 1,
+    vm = 10,
+    g56 = 100,
+    ZgwE = 33333,
+    gE3Y95MDwl = 33333e10,
+    lYfo = {1, 2},
+    kQcn = {0, 2},
+    kQc6cV = {0, 0, 2},
+    oYxu6xh3ytXMO6 = {123, 456, 789, 1000000},
+  },
+
+  ['foo bar'] = {
+    N4 = 0,
+    gr = 1,
+    bG = 25,
+    NqQ7g7dn = 10e10,
+    WZi3UB = {1, 2, 3},
+    N4s7sQspsv = {0, 0, 0, 0, 0},
+  },
+
 }
--- spec/hashids_spec.lua
+++ spec/hashids_spec.lua
@@ -5,32 +5,56 @@ local fixtures = require('fixtures')
 unpack = unpack or table.unpack
 
 
-describe('test hashids', function()
+local function table_repr(tbl)
+  return '{' .. table.concat(tbl, ', ') .. '}'
+end
 
-  it('can import clib.so', function()
+
+describe("test hashids", function()
+
+  it("can import clib.so", function()
     assert.has_no.errors(function() require('hashids.clib') end)
     assert.is.equal(type(require('hashids.clib')), 'table')
   end)
 
-  describe('encode using default params', function()
-
-    local hids = hashids.new()
-
-    for hash, numbers in pairs(fixtures) do
-      local numbers_repr
-      if type(numbers) == 'table' then
-        numbers_repr = '{' .. table.concat(numbers, ', ') .. '}'
-      else
-        numbers_repr = tostring(numbers)
-        numbers = {numbers}
-      end
-
-      it(('should encode %s to %s'):format(numbers_repr, hash), function()
-        assert.is.equal(hids:encode(unpack(numbers)), hash)
-      end)
-
-    end
-
+  it("omitting salt arg is equivalent to empty salt string", function()
+    local hids_omit = hashids.new()
+    local hids_empty = hashids.new('')
+    assert.is.equal(hids_omit.salt, '')
+    assert.is.equal(hids_omit:encode(100), hids_empty:encode(100))
   end)
+
+  for salt, fixture in pairs(fixtures) do
+
+    describe(("encode using salt '%s'"):format(salt), function()
+      local hids = hashids.new(salt)
+      for hash, numbers in pairs(fixture) do
+        local numbers_repr
+        if type(numbers) == 'table' then
+          numbers_repr = table_repr(numbers)
+        else
+          numbers_repr = tostring(numbers)
+          numbers = {numbers}
+        end
+        it(("should encode %s to '%s'"):format(numbers_repr, hash), function()
+          assert.is.equal(hids:encode(unpack(numbers)), hash)
+        end)
+      end
+    end)
+
+    describe(("decode using salt '%s'"):format(salt), function()
+      local hids = hashids.new(salt)
+      for hash, numbers in pairs(fixture) do
+        if type(numbers) == 'number' then
+          numbers = {numbers}
+        end
+        local numbers_repr = table_repr(numbers)
+        it(("should decode '%s' to %s"):format(hash, numbers_repr), function()
+          assert.is.same(hids:decode(hash), numbers)
+        end)
+      end
+    end)
+
+  end
 
 end)
